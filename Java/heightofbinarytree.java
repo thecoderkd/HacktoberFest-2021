37c40aaaf77b2636dded082e8a311a9edab15d59
saurav Kumar <saurav12112000@gmail.com>
2021-10-03 14:28:22 +0530

    added code for height of binary tree

---
+++ Java/heightofbinarytree.java
@@ -0,0 +1,60 @@
+import java.util.*;
+import java.io.*;
+
+class Node {
+    Node left;
+    Node right;
+    int data;
+    
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class Main {
+	public static int height(Node root) {
+          if(root==null) return 0;
+          if(root.left==null && root.right==null) return 0;
+          int left = height(root.left);
+          int right = height(root.right);
+          return Math.max(left,right)+1;
+    }
+
+	public static Node insert(Node root, int data) {
+        if(root == null) {
+            return new Node(data);
+        } else {
+            Node cur;
+            if(data <= root.data) {
+                cur = insert(root.left, data);
+                root.left = cur;
+            } else {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        Node root = null;
+        while(t-- > 0) {
+            int data = scan.nextInt();
+            root = insert(root, data);
+        }
+        scan.close();
+        int height = height(root);
+        System.out.println(height);
+    }	
+}
+/*
+Input:
+7
+3 5 2 1 4 6 7
+Output:
+3
+*/
